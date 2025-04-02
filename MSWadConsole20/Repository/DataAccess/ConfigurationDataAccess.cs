97135505389c2704a3bb0cc033b4f867c088c95d
MatteoMantSI <matteo.mantione@sistinf.it>
2025-04-02 18:18:01 +0200

    modificato metodo GetLibrary con sqlParameter

--- MSWadConsole20/Repository/DataAccess/ConfigurationDataAccess.cs
+++ MSWadConsole20/Repository/DataAccess/ConfigurationDataAccess.cs
@@ -2,6 +2,9 @@
 using MSWadConsole20.Repository.DataModel;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using Azure.Core;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace MSWadConsole20.Repository.DataAccess
 {
@@ -28,10 +31,20 @@ namespace MSWadConsole20.Repository.DataAccess
         }
 
         public virtual LibraryData? GetLibrary(LibraryRequest request)
-        {
-            var res = Database.SqlQuery<LibraryData>($"EXEC [dbo].[sp_Library_Select] @LibreriaApplicazioneID, @CodiceDimensions, @CodiceWAD, @DescrizioneLibreria, @ConDisabilitati, @Contesto, @CodiceFiscale")?.FirstOrDefault();
+        {         
+            var libreriaApplicazioneIdParam = new SqlParameter() { ParameterName = "@LibreriaApplicazioneID", Value = request.LibreriaApplicazioneID };
+            var codiceWadParam = new SqlParameter() { ParameterName = "@CodiceWAD", Value = request.CodiceWAD };
+            var codiceDimensionsParam = new SqlParameter() { ParameterName = "@CodiceDimensions", Value = request.CodiceDimensions };
+            var descrizioneLibreriaParam = new SqlParameter() { ParameterName = "@DescrizioneLibreria", Value = request.DescrizioneLibreria };
+            var conDisabilitatiParam = new SqlParameter() { ParameterName = "@ConDisabilitati", Value = request.ConDisabilitati };
+            var contestoParam = new SqlParameter() { ParameterName = "@Contesto", Value = request.Contesto };
+            var codiceFiscaleParam = new SqlParameter() { ParameterName = "@CodiceFiscale", Value = request.CodiceFiscale };            
 
-            return res;
+            var response = Database.SqlQuery<LibraryData>($"[dbo].[sp_Library_Select] {libreriaApplicazioneIdParam},{codiceWadParam},{codiceDimensionsParam},{descrizioneLibreriaParam},{conDisabilitatiParam},{contestoParam},{codiceFiscaleParam}").AsEnumerable<LibraryData>().FirstOrDefault();
+
+            //var res = Database.SqlQuery<LibraryData>($"EXEC [dbo].[sp_Library_Select] @LibreriaApplicazioneID, @CodiceDimensions, @CodiceWAD, @DescrizioneLibreria, @ConDisabilitati, @Contesto, @CodiceFiscale")?.FirstOrDefault();
+
+            return response;
         }
 
         public virtual List<LibraryData>? GetLibraries(LibraryRequest request)
